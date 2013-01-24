0847cdde022c07df2d6fbb34cafd40ab0338dab6
tt-09 <tt-09@tt-09.(none)>
2013-01-24 11:34:23 +0530

    latest changes commit

---
+++ db/migrate/20100525122220_create_meters.rb
@@ -0,0 +1,15 @@
+class CreateMeters < ActiveRecord::Migration
+  def self.up
+    create_table :meters do |t|
+      t.string :meter_identifier
+      t.string :type_of_meter
+      t.string :meter_ip
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :meters
+  end
+end
